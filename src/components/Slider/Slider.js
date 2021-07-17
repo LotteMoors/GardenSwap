37c1945b21bc460862403319fb6116932ff3425b
LotteMoors <lotte-moors@hotmail.com>
2021-07-18 01:24:55 +0200

    check if response image is undefined

--- src/components/Slider/Slider.js
+++ src/components/Slider/Slider.js
@@ -4,13 +4,17 @@ import Carousel from "react-bootstrap/Carousel";
 const Slider = ({ res }) => {  
 
   return (
-    <Carousel className="w-0" slide={false} fade={false}>
+    <>
+    {res !== undefined? 
+    <Carousel className="w-0" slide={false} fade={false} interval={null}>
       {res.map((img, index) => (
         <Carousel.Item style={{width:"20em", maxHeight:'25em'}} key={index}>
           <img height="300" width="320" src={img.url} alt="" />
         </Carousel.Item>
       ))}
     </Carousel>
+    : null}
+    </>
   );
 };
 
